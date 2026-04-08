cd7d86a26b674eaf625fdf8eb17090e6a04c574c
wadoudramanou5-max <wadoudramanou5@gmail.com>
2026-04-08 16:49:40 +0100

    Add click event to start button for navigation

---
+++ home.js
@@ -0,0 +1,7 @@
+let startbtn = document.getElementById("start");
+
+if (startbtn) {
+    startbtn.addEventListener("click", function() {
+        window.location.href = "Dashboard.html";
+    });
+}
